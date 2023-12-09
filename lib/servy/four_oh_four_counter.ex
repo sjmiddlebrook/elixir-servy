7edb95dd5acd0de7e762a470261cee97b445ff50
Jack Middlebrook <jack.middlebrook@gmail.com>
2023-12-09 16:40:59 +0800

    use gen server

--- lib/servy/four_oh_four_counter.ex
+++ lib/servy/four_oh_four_counter.ex
@@ -1,47 +1,48 @@
 defmodule Servy.FourOhFourCounter do
-  alias Servy.GenericServer
   @name __MODULE__
+
+  use GenServer
 
   def start(initial_state \\ %{}) do
     if Mix.env() != :test do
       IO.puts("\nStarting 404 counter server")
     end
 
-    GenericServer.start(__MODULE__, initial_state, @name)
+    GenServer.start(__MODULE__, initial_state, name: @name)
   end
 
   def bump_count(path) do
-    GenericServer.call(@name, {:bump_count, path})
+    GenServer.call(@name, {:bump_count, path})
   end
 
   def get_counts() do
-    GenericServer.call(@name, :get_counts)
+    GenServer.call(@name, :get_counts)
   end
 
   def get_count(path) do
-    GenericServer.call(@name, {:get_count, path})
+    GenServer.call(@name, {:get_count, path})
   end
 
   def reset() do
-    GenericServer.cast(@name, :reset)
+    GenServer.cast(@name, :reset)
   end
 
-  def handle_call({:bump_count, path}, state) do
+  def handle_call({:bump_count, path}, _from, state) do
     new_state = Map.update(state, path, 1, &(&1 + 1))
     count = Map.get(new_state, path)
-    {count, new_state}
+    {:reply, count, new_state}
   end
 
-  def handle_call(:get_counts, state) do
-    {state, state}
+  def handle_call(:get_counts, _from, state) do
+    {:reply, state, state}
   end
 
-  def handle_call({:get_count, path}, state) do
+  def handle_call({:get_count, path}, _from, state) do
     count = Map.get(state, path, 0)
-    {count, state}
+    {:reply, count, state}
   end
 
   def handle_cast(:reset, _state) do
-    %{}
+    {:noreply, %{}}
   end
 end
